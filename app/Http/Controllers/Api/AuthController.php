4c35508ac5c3ba1cc4cc83824056a2bf6e1aab34
DavitAbrahamyan <davit.abrahamyan@list.ru>
2022-08-22 14:03:58 +0400

    Социальная сеть Laravel 8 + Vue.js 3 Урок 4 - LARAVEL OAuth2 PASSWORD

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,14 +4,38 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Laravel\Passport\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    public function login(){
+    public function login(Request $request){
 
+        $validator = Validator::make($request->all(), [
+            'email' => 'required | string | email | max:255',
+            'password' => 'required | between:8, 255',
+        ]);
+
+        if($validator->fails()) {
+            return response(['errors' => $validator->errors()->all()], 422);
+        }
+
+        $passwordGrantClient = Client::where('password_client', 1)->first();
+
+        $data = [
+          'grant_type' =>  'password',
+          'client_id' =>   $passwordGrantClient->id,
+            'client_secret' => $passwordGrantClient->secret,
+            'username' => $request->email,
+            'password' => $request->password,
+            'scope' => '*'
+        ];
+
+        $tokenRequest = Request::create('/oauth/token', 'post', $data);
+
+        return app()->handle($tokenRequest);
     }
 
     public function register(Request $request){
