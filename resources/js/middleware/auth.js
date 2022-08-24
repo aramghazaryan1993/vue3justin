c56a950baf11a2b292f878f6f9634f2233c74fde
DavitAbrahamyan <davit.abrahamyan@list.ru>
2022-08-24 14:04:41 +0400

    Социальная сеть Laravel 8 + Vue.js 3 Урок 8 - Navigation Guards

--- resources/js/middleware/auth.js
+++ resources/js/middleware/auth.js
@@ -1,10 +1,8 @@
 export default function ({next, store}) {
 
-    if (!store.getters.auth.loggedIn)
+    if (!store.getters.auth.loggedIn || !store.getters.auth().token)
     {
-        return next({
-            name: 'dashboard'
-        })
+        return next('login')
     }
     return next()
 }
